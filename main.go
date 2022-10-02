703157285491c2b3ef6404a33786a7ba5cb31115
Vsevolod <sevakoltypin@gmail.com>
2022-10-02 16:52:18 +0300

    добавил пару структур внутри стурктур

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/koltypka/SMS/rand"
+	"github.com/koltypka/SMS/system"
 )
 
 func main() {
-	fmt.Print(rand.MakeRavnr())
+	fmt.Print(rand.MakeRavnr(7))
 	fmt.Print("\n")
+
+	fmt.Print(system.MakeVisitors())
 }
--- rand/rand.go
+++ rand/rand.go
@@ -5,25 +5,13 @@ import (
 	"time"
 )
 
-//1-5 каналов
-//каждые 7 минут показательно прибывают покупатели
-//каждые 9 минут показательно обслуживаем
-//в среднем покупатели покупают на сумму 500 рублей
-//время функционирования системы 8 часов
-func Test() {
-
-}
-
 //генератор случайных чисел работает только внутри тела функции
-func MakeRavnr() float64 {
+func MakeRavnr(lambda float64) uint16 {
 	//задаём семя
 	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
 	//генерируем значение
-	returnFloat64 := generator.ExpFloat64()
+	returnFloat64 := 1 / (generator.ExpFloat64() / lambda)
 
-	return returnFloat64
-}
-
-func main() {
+	return uint16(returnFloat64)
 
 }
